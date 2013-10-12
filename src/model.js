c9152026988321564a1b7980d22d4308f884ed66
Jason Hinebaugh <jason@bootjackdesign.com>
2013-10-12 17:43:10 -0400

    Initialize fields as empty array
    The fields variable in model.js is added to Model.prototype after
    the Model() constructor function is defined.

--- src/model.js
+++ src/model.js
@@ -12,6 +12,7 @@ module.exports = function (name, schema) {
         'date': {name: 'date', default: new Date().toUTCString()}
     };
     
+    fields = [];
     mongoSchema = {};
     for (property in schema) {
         if (schema.hasOwnProperty(property) && schema[property].type) {
